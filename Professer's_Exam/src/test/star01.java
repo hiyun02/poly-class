39eeb49f100e0b032e113f96269682049208d123
hayoon020105 <psalter150@naver.com>
2021-04-19 15:10:44 +0900

    시험대비 별찍기 유형 연습 보완

--- Professer's_Exam/src/test/star01.java
+++ Professer's_Exam/src/test/star01.java
@@ -7,18 +7,17 @@ public class star01 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
-		for (int i = 1; i <= 2 * n - 1; i++) {
+		int k = 0;
+		for (int i = 0; i < 2 * n - 1; i++) {
 			if (i < n) {
-				for (int j = 1; j <= i; j++) {
-					System.out.print("*");
-				}
+				k++;
+			} else {
+				k--;
+			}
+			for (int j = 0; j < k; j++) {
+				System.out.print("*");
 			}
 
-			else {
-				for (int j = n; j > i-n; j--) {
-					System.out.print("*");
-				}
-			}
 			System.out.println();
 		}
 	}
--- Professer's_Exam/src/test/star02.java
+++ Professer's_Exam/src/test/star02.java
@@ -7,24 +7,21 @@ public class star02 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
+		int k;
 		for (int i = 1; i <= 2 * n - 1; i++) {
 			if (i < n) {
-				for(int j=0;j<n-i;j++) {
-					System.out.print(" ");
-				}
-				for (int j = 0; j < i; j++) {
-					System.out.print("*");
-				}
+				k = n - i;
+			} else {
+				k = i - n;
 			}
 
-			else {
-				for(int j=0;j<i-n;j++) {
-					System.out.print(" ");
-				}
-				for (int j = 0; j<2*n-i; j++) {
-					System.out.print("*");
-				}
+			for (int j = 0; j < k; j++) {
+				System.out.print(" ");
 			}
+			for (int j = 0; j < n - k; j++) {
+				System.out.print("*");
+			}
+
 			System.out.println();
 		}
 	}
