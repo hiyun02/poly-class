371c881770c96135ef01aec09728b43f521a99d8
hayoon020105 <psalter150@naver.com>
2021-04-20 19:53:39 +0900

    자바 시험 대비 : 별찍기2 보완

--- Professer's_Exam/src/test/star02.java
+++ Professer's_Exam/src/test/star02.java
@@ -1,29 +1,30 @@
 package test;
 
-import java.util.Scanner;
-
 public class star02 {
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int k;
-		for (int i = 1; i <= 2 * n - 1; i++) {
-			if (i < n) {
-				k = n - i;
-			} else {
-				k = i - n;
-			}
+		   public static void main(String[] args) {
+			      int k = 5;// 찍을 공백의 개수
 
-			for (int j = 0; j < k; j++) {
-				System.out.print(" ");
-			}
-			for (int j = 0; j < n - k; j++) {
-				System.out.print("*");
-			}
+			      for (int i = 0; i < 9; i++) {
+			         if (i < 5) {
+			            k--;// 5번째 줄까지는 공백의 개수가 1씩 줄지만
+			         } else {
+			            k++;// 그 이후에는 공백의 개수가 1씩 감소한다
+			         }
 
-			System.out.println();
+			         // 줄마다 항상 5개의 문자를 출력
+			         for (int j = 0; j < 5; j++) {
+			        	//별은 k번만 출력
+			        	 System.out.print(j<k ? " " : "*");
+				
+//				  if (j < k)System.out.println("*");
+//				  else System.out.print(" ");
+				 
+			         }
+
+			         System.out.println();
+			      }
+			   }
+
 		}
-	}
 
-}
