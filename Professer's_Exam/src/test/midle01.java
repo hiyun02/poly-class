727d81b2df369b29a5d5402467c9fc123b142f92
hayoon020105 <psalter150@naver.com>
2021-04-28 17:48:11 +0900

    중간고사 리뷰

---
+++ Professer's_Exam/src/test/midle01.java
@@ -0,0 +1,17 @@
+package test;
+
+public class midle01 {
+	public static void main(String[] args) {
+		int num=10;
+		System.out.print(num+"은 ");
+		if(num%2==0) {
+			System.out.println("짝수입니다");
+		}
+		else {
+			System.out.println("홀수입니다");
+		}
+		
+		
+	}
+
+}
---
+++ Professer's_Exam/src/test/midle02.java
@@ -0,0 +1,14 @@
+package test;
+
+public class midle02 {
+
+	public static void main(String[] args) {
+
+		for(int i=3;i<9;i++) {
+			for(int j=1;j<10;j++) {
+				System.out.println(i+"X"+j+"="+(i*j));
+			}
+		}
+	}
+
+}
